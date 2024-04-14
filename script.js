07e0f7ee7f111bae6a14ee483deca42454d3abf2
Shruti-Vaidya <147685533+Shruti-dotcom-01@users.noreply.github.com>
2024-04-14 17:06:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const form = document.getElementById("form1");
+
 function getFormvalue() {
     //Write your code here
-
+  const fname = form.fname.value; //the </input>
+	const lname = form.lname.value  ;
+  alert(fname + " "+lname)
 }
